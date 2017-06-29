33014ca4d8f829f6d63d4ab207239c9a8df7bfab
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-06-29 16:25:30 +0300

    Pass linkerOpts from .def to klib manifest.

--- Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/jvm/main.kt
+++ Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/jvm/main.kt
@@ -30,7 +30,6 @@ fun main(args: Array<String>) {
             "arch" to (System.getenv("TARGET_ARCH") ?: "x86-64"),
             "os" to (System.getenv("TARGET_OS") ?: host)
     )
-
     processLib(konanHome, substitutions, parseArgs(args))
 }
 
@@ -256,11 +255,17 @@ private fun loadProperties(file: File?, substitutions: Map<String, String>): Pro
     return result
 }
 
-private fun parseDefFile(file: File?, substitutions: Map<String, String>): Pair<Properties, List<String>> {
+private fun Properties.storeProperties(file: File) {
+    file.outputStream().use { 
+        this.store(it, null) 
+    }
+}
+
+private fun parseDefFile(file: File?, substitutions: Map<String, String>): Triple<Properties, Properties, List<String>> {
     val properties = Properties()
 
     if (file == null) {
-        return properties to emptyList()
+        return Triple(properties, Properties(), emptyList())
     }
 
     val lines = file.readLines()
@@ -281,9 +286,26 @@ private fun parseDefFile(file: File?, substitutions: Map<String, String>): Pair<
 
     val propertiesReader = StringReader(propertyLines.joinToString(System.lineSeparator()))
     properties.load(propertiesReader)
+
+    // We pass through unsubstituted properties.
+    val manifestAddendProperties = passThrough(properties)
+
     substitute(properties, substitutions)
 
-    return properties to headerLines
+    return Triple(properties, manifestAddendProperties, headerLines)
+}
+
+private fun propertyIsPassThrough(key: String): Boolean = key.startsWith("linkerOpts")
+
+// Select properties we pass from `.def` to compiler `-maniest`.
+private fun passThrough(properties: Properties): Properties {
+    val passThroughProperties = Properties()
+    for (key in properties.stringPropertyNames()) {
+        if (propertyIsPassThrough(key)) {
+            passThroughProperties.setProperty(key, properties.getProperty(key))
+        }
+    }
+    return passThroughProperties
 }
 
 private fun usage() {
@@ -316,13 +338,15 @@ private fun processLib(konanHome: String,
     val flavor = KotlinPlatform.values().single { it.name.equals(flavorName, ignoreCase = true) }
     val target = args["-target"]?.single()?.targetSuffix() ?: defaultTarget
     val defFile = args["-def"]?.single()?.let { File(it) }
+    val manifestAddend = args["-manifest"]?.single()?.let { File(it) }
 
     if (defFile == null && args["-pkg"] == null) {
         usage()
         return
     }
 
-    val (config, defHeaderLines) = parseDefFile(defFile, substitutions)
+    val (config, manifestAddendProperties, defHeaderLines) 
+        = parseDefFile(defFile, substitutions)
 
     val konanFileName = args["-properties"]?.single() ?:
             "${konanHome}/konan/konan.properties"
@@ -408,6 +432,9 @@ private fun processLib(konanHome: String,
             gen.generateFiles(ktFile = ktFile, cFile = cFile, entryPoint = entryPoint)
         }
     }
+
+    manifestAddend?.parentFile?.mkdirs()
+    manifestAddend ?.let { manifestAddendProperties.storeProperties(it) }
 
     val workDir = defFile?.absoluteFile?.parentFile ?: File(userDir)
 
